f521569827ec2d3fdb958916c9edd3fd89aeba6d
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-23 11:25:19 +0700

    Create layouts

---
+++ features/manage-products/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ManageProducts = () => {
+    return (
+        <div>
+            <h1>Nội dung quản lý</h1>
+        </div>
+    );
+};
+
+export default ManageProducts;
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import AdminLayout from "../../components/admin-layout";
 
 const Admin = () => {
     return (
         <div>
             <h1>Page Admin</h1>
+            {/* gọi các components */}
         </div>
     );
 };
-
+Admin.getLayout = function getLayout(page) {
+    return <AdminLayout>{page}</AdminLayout>
+}
 export default Admin;
